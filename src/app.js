e661d66c65af814baee0ca7f216431a501915454
Nabeel Farooq <nabeel007dar@gmail.com>
2025-04-13 16:23:27 +0530

    Set up the project skeleton

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+
+
+
+
+
+
+const app = express();
+app.use("/",(req,res) => {
+    res.send("byeeeeeeeeee");
+});
+app.use("/test",(req,res) => {
+    res.send("Hello from the server");
+});
+app.use("/hello",(req,res) => {
+    res.send("Hello hello hello");
+});
+app.listen(7777, ()=>{
+    console.log("Server is successfully listening on port 7777............");
+});
+
+
+
