0630b6151d21ab771991d78bceaca5cc241aa9fe
Nabeel Farooq <nabeel007dar@gmail.com>
2025-04-13 20:02:43 +0530

    Explore GET,POST,DELETE

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
-const express = require('express');
-
-
-
-
-
-
+const express = require("express");
 
 const app = express();
-app.use("/",(req,res) => {
-    res.send("byeeeeeeeeee");
+
+app.use("/user", (req, res) => {
+  res.send("hahahahahaahahha");
 });
-app.use("/test",(req,res) => {
-    res.send("Hello from the server");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Nabeel", lastName: "Farooq" });
 });
-app.use("/hello",(req,res) => {
-    res.send("Hello hello hello");
+app.post("/user", (req, res) => {
+  // saving data to db
+  res.send("Data successfully saved to the database");
 });
-app.listen(7777, ()=>{
-    console.log("Server is successfully listening on port 7777............");
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
+});
+app.use("/test", (req, res) => {
+  res.send("Hello from the server");
 });
 
-
-
+app.listen(7777, () => {
+  console.log("Server is successfully listening on port 7777............");
+});
