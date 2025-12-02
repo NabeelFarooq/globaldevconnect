1081b37ed4bb711e8b83b3074cec9114edb27a42
Nabeel Farooq <nabeel007dar@gmail.com>
2025-12-02 18:07:45 +0530

    chore: improve auth error message

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ const userAuth = async (req, res, next) => {
     // read the token from the req cookies
     const { token } = req.cookies;
     if (!token) {
-      throw new Error("Token is not valid!!!!!!!!!!!!!!!");
+      return res.status(401).send("Please login!");
     }
     // validate the token
     const decodedObj = await jwt.verify(token, "DEV@Connect$77");
