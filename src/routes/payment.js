7c884fc75785dddca3068c0e74646218505ac423
Nabeel Farooq <nabeel007dar@gmail.com>
2025-12-15 10:47:20 +0530

    feat: integrate Razorpay payment gateway for premium membership subscriptions

---
+++ src/routes/payment.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const { userAuth } = require("../middlewares/auth");
+const paymentRouter = express.Router();
+const razorpayInstance = require("../utils/razorpay");
+const paymentModel = require("../models/payment");
+const { membershipAmount } = require("../utils/constants");
+const {
+  validateWebhookSignature,
+} = require("razorpay/dist/utils/razorpay-utils");
+const user = require("../models/user");
+paymentRouter.post("/payment/create", userAuth, async (req, res) => {
+  try {
+    const { membershipType } = req.body;
+    const { firstName, lastName, emailId } = req.user;
+
+    const order = await razorpayInstance.orders.create({
+      amount: membershipAmount[membershipType] * 100,
+      currency: "INR",
+      receipt: "receipt#1",
+      notes: {
+        firstName,
+        lastName,
+        emailId,
+        membershipType: membershipType,
+      },
+    });
+    console.log(order);
+
+    const payment = new paymentModel({
+      userId: req.user._id,
+      orderId: order.id,
+      status: order.status,
+      amount: order.amount,
+      currency: order.currency,
+      receipt: order.receipt,
+      notes: order.notes,
+    });
+    const savedPayment = await payment.save();
+
+    res.json({ ...savedPayment.toJSON(), keyId: process.env.RAZORPAY_KEY_ID });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+});
+
+paymentRouter.post("/payment/verify", async (req, res) => {
+  try {
+    const webhookSignature = req.get["X-Razorpay-Signature"];
+
+    const isWebhookValid = validateWebhookSignature(
+      JSON.stringify(req.body),
+      webhookSignature,
+      process.env.RAZORPAY_WEBHOOK_SECRET
+    );
+
+    if (!isWebhookValid) {
+      return res.status(400).json({ message: "Invalid webhook signature" });
+    }
+
+    const payment = await paymentModel.findOne({
+      orderId: req.body.payload.payment.entity.order_id,
+    });
+
+    payment.status = req.body.payload.payment.entity.status;
+    await payment.save();
+
+    const user = await user.findOne({ _id: payment.userId });
+    user.isPremium = true;
+    user.membershipType = payment.notes.membershipType;
+    await user.save();
+
+    // if (req.body.event === "payment.captured") {
+    // }
+    // if (req.body.event === "payment.failed") {
+    // }
+
+    return res.status(200).json({ message: "Webhook received successfully" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = paymentRouter;
